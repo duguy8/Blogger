92e85b3a6a608bfbb41254eda41b77bb180e587f
Kris Litman <duguy8@gmail.com>
2021-02-19 23:17:23 -0500

    Added test for each article showing its comments

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -27,5 +27,22 @@ describe "user sees all articles" do
 
       expect(page).to have_link("Create a New Article")
     end
+
+    it "displays information for one article" do
+      article = Article.create!(title: "New Title", body: "New Body")
+      comment_1 = article.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article.comments.create(author_name: "You", body: "So much to say")
+
+      visit articles_path
+
+      click_link article.title
+
+      expect(page).to have_content(article.title)
+      expect(page).to have_content(article.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
+    end
   end
 end
