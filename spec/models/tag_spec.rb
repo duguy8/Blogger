1cf2436ddc7ac1baa5cdbb07440cce4470c9905b
Kris Litman <duguy8@gmail.com>
2021-02-20 15:53:01 -0500

    Added tags, tagging and tests for the models

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+describe Tag, type: :model do
+  describe "validations" do
+    it {should validate_presence_of(:name)}
+  end
+
+  describe "relationships" do
+    it {should have_many(:taggings)}
+    it {should have_many(:articles).through(:taggings)}
+  end
+end
