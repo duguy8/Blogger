5d0c1371c7a4a2a3a15982b47170da7cbf5ba79c
Kris Litman <duguy8@gmail.com>
2021-02-19 22:11:55 -0500

    Added destroy action to controller

--- spec/features/article_show_spec.rb
+++ spec/features/article_show_spec.rb
@@ -17,7 +17,7 @@ describe "user sees one article" do
       article_1 = Article.create!(title: "Title 1", body: "Body 1")
       article_2 = Article.create!(title: "Title 2", body: "Body 2")
 
-      visit articles_path(article1)
+      visit article_path(article_1)
 
       expect(page).to have_link("Delete Article")
 
@@ -25,7 +25,7 @@ describe "user sees one article" do
 
       expect(current_path).to eq(articles_path)
       expect(page).to have_content(article_2.title)
-      expect(page).not_to have_content(article1.title)
+      expect(page).not_to have_content(article_1.title)
     end
   end
 end
