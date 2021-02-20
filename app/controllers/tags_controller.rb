4f1802c22ada6b2897ae4963ed5a1c64dbb3ec12
Kris Litman <duguy8@gmail.com>
2021-02-20 16:42:09 -0500

    Refactored tags controller and finished iteration 3

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,21 @@
 class TagsController < ApplicationController
+  before_action :set_tag, only: [:show, :destroy]
   def index
     @tags = Tag.all
   end
 
   def show
+  end
+
+  def destroy
+    @tag.destroy
+
+    redirect_to(tags_path)
+  end
+
+  private
+
+  def set_tag
     @tag = Tag.find(params[:id])
   end
 end
--- spec/features/tag_index_spec.rb
+++ spec/features/tag_index_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe "When I visit the tag index page" do
     expect(page).to have_content(tag2.name)
     expect(page).to have_content(tag3.name)
   end
+
+  it "Has a link to delete each tag" do
+    tag1 = Tag.create!(name: "tag1")
+    tag2 = Tag.create!(name: "tag2")
+    tag3 = Tag.create!(name: "tag3")
+
+    visit tags_path
+
+    click_link('Delete tag1')
+    expect(page).to have_content(tag2.name)
+    expect(page).to have_content(tag3.name)
+    expect(page).not_to have_content(tag1.name)
+  end
 end
